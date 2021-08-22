afec7047f286e3948547d9a1c02fb3c6134a36cf
Fazel Saeedi <fazels36@gmail.com>
2021-08-22 13:53:46 +0430

    Part 1 1. using declaration 2. write to console

---
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*|--------------------------------------------------
+            |                                                 |
+            |                    Part 1                       |
+            |            Using System declaration             |
+            |                                                 |
+            |-------------------------------------------------|
+            |                                                 |
+            |                                                 |
+            |     The namespace declaration . usingSystem     |  
+            |     indicates that you are using the system     |
+            |     namespace                                   |
+            |                                                 |
+            |     A namespace is used to organize your code   |
+            |     and is collection of classes , interfaces   |
+            |     ,structs , enums and delegates.             |   
+            |                                                 |
+            |-------------------------------------------------|
+            |                                                 |
+            |     Main method is the entry point into your    |
+            |      application                                |
+            |                                                 |
+            ---------------------------------------------------*/
+
+
+            // Write to console
+            System.Console.WriteLine("Hello world");
+
+
+            // End -----------------------------------------------------------------------
+        }
+    }
+}
+
+
