d4538b63d6c395a91f55908a5f76a1c875d52e03
Fazel Saeedi <fazels36@gmail.com>
2021-08-22 14:05:56 +0430

    1. Reading from the console 2. Writing to the console    a . concatenation    b . place holder syntax

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -8,35 +8,69 @@ namespace CSharpTutorial
         {
 
 
-            /*|--------------------------------------------------
-            |                                                 |
-            |                    Part 1                       |
-            |            Using System declaration             |
-            |                                                 |
-            |-------------------------------------------------|
-            |                                                 |
-            |                                                 |
-            |     The namespace declaration . usingSystem     |  
-            |     indicates that you are using the system     |
-            |     namespace                                   |
-            |                                                 |
-            |     A namespace is used to organize your code   |
-            |     and is collection of classes , interfaces   |
-            |     ,structs , enums and delegates.             |   
-            |                                                 |
-            |-------------------------------------------------|
-            |                                                 |
-            |     Main method is the entry point into your    |
-            |      application                                |
-            |                                                 |
-            ---------------------------------------------------*/
+            /*|-------------------------------------------------|
+              |                                                 |
+              |                    Part 1                       |
+              |            Using System declaration             |
+              |                                                 |
+              |-------------------------------------------------|
+              |                                                 |
+              |                                                 |
+              |     The namespace declaration . usingSystem     |  
+              |     indicates that you are using the system     |
+              |     namespace                                   |
+              |                                                 |
+              |     A namespace is used to organize your code   |
+              |     and is collection of classes , interfaces   |
+              |     ,structs , enums and delegates.             |   
+              |                                                 |
+              |-------------------------------------------------|
+              |                                                 |
+              |     Main method is the entry point into your    |
+              |      application                                |
+              |                                                 |
+              ---------------------------------------------------*/
 
 
             // Write to console
-            System.Console.WriteLine("Hello world");
+            //System.Console.WriteLine("Hello world");
 
 
-            // End -----------------------------------------------------------------------
+            // End Part 1-----------------------------------------------------------------------
+
+
+            /*|--------------------------------------------------
+              |                                                 |
+              |                    Part 2                       |
+              |                                                 |
+              |-------------------------------------------------|
+              |                                                 |
+              |                                                 |
+              |           Reading from the console              | 
+              |                                                 |            
+              |                                                 |            
+              |           Writing to the console                |
+              |              a. concatenation                   |
+              |              a. Place holder syntax             |
+              |                                                 |              |                                                 |
+              |                                                 |              |                                                 |
+              |-------------------------------------------------|*/
+
+            Console.WriteLine("Please enter your name");
+
+
+            // reading from the console (concatenation)
+            string FirstName = Console.ReadLine();
+            string LastName  = Console.ReadLine();
+
+
+            // reading from the console (Place holder syntax)
+            Console.WriteLine("Hello {0},{1}" , FirstName , LastName );
+
+
+
+
+            // End Part 2-----------------------------------------------------------------------
         }
     }
 }
