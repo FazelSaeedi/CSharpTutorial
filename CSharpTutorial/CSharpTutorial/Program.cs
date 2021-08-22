b534d278514766a4b83ede9b14ab3be71a910292
Fazel Saeedi <fazels36@gmail.com>
2021-08-22 15:58:12 +0430

    Part 7 1. implicit & explicit conversion 2. Pars && Try Parse

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -117,27 +117,197 @@ namespace CSharpTutorial
               |-------------------------------------------------|*/
 
 
-            string Name = "Fazel";
-            Console.WriteLine(Name);
+            // string Name = "Fazel";
+            // Console.WriteLine(Name);
 
-            Name = "\"Fazel\"";
-            Console.WriteLine(Name);
 
-            Name = "One\nTwo\nThree";
-            Console.WriteLine(Name);
+            // Name = "\"Fazel\"";
+            // Console.WriteLine(Name);
 
-            Name = "C:\\System\\Csharp\\Opp";
-            Console.WriteLine(Name);
 
-            // Verbatim
-            Name = @"C:\\System\\Csharp\\Opp";
-            Console.WriteLine(Name);
+            // Name = "One\nTwo\nThree";
+            // Console.WriteLine(Name);
 
-            Name = @"C:\System\Csharp\Opp";
-            Console.WriteLine(Name);
+
+            // without Verbatim
+            // Name = "C:\\System\\Csharp\\Opp";
+            // Console.WriteLine(Name);
+
+
+            // with Verbatim
+            // Name = @"C:\System\Csharp\Opp";
+            // Console.WriteLine(Name);
+
 
             // End Part 4-----------------------------------------------------------------------
 
+
+            /*|--------------------------------------------------
+              |                                                 |
+              |                     Part 5                      |
+              |                Common operator                  |
+              |                                                 |
+              |-------------------------------------------------|
+              |                                                 |
+              |                                                 |
+              |       Assignment - Arthimetic - Comparison      |
+              |                   Conditional                   |
+              |                                                 |
+              |                                                 |             
+              |-------------------------------------------------|*/
+
+            // int i = 10 ;
+            // int s = 2;
+            // 
+            // int Result = i * s; 
+            // Result = i / s; 
+            // Result = i % s; 
+            // Result = i - s;
+            // Result = i + s;
+            // 
+            // int Number = 10;
+
+
+            // !=
+            // &&
+            // ||
+            // <
+            // >
+            // >=
+            // <=
+            // if(Number != 10 )
+            // {
+            // 
+            // }
+
+            // ? -> if 
+            // : -> else
+
+            // bool IsNumber10 = Number == 10 ? true : false ;
+
+
+            // End Part 5-----------------------------------------------------------------------
+
+
+            /*|--------------------------------------------------
+              |                                                 |
+              |                     Part 6                      |
+              |                 NUllable Types                  |
+              |                                                 |             
+              |-------------------------------------------------|
+              |                                                 |
+              | Value Types -> int , float , double , enums etc |                                       |
+              |                                                 |
+              |      Reference Types -> interface , Class       |
+              |              , delegates , arrays               |     
+              |                                                 |             
+              |            Null Coalescing Operator             |             
+              |                                                 |             
+              |-------------------------------------------------|*/
+
+            // by default value types are non nullable
+
+            // int i = null ; it is false syntax
+
+            // bool? AreYouMajor = null;
+            // if (AreYouMajor == true) // true
+            // {
+            //     Console.WriteLine("user is  major ");
+            // }
+            // else if(!AreYouMajor.Value) // null
+            // {
+            //     Console.WriteLine("user is not major ");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("user is did not answer the question ");
+            // }
+            // 
+            // 
+            // int? TicketsOnSale = 100;
+            // // int? TicketsOnSale = null;
+            // 
+            // int AvaialableTicket;
+            // 
+            // if (TicketsOnSale == null)
+            // {
+            //     AvaialableTicket = 0;
+            // }
+            // else
+            // {
+            //     AvaialableTicket = (int)TicketsOnSale;
+            // }
+            // 
+            // Console.WriteLine("AvaialableTicket = {0} " , AvaialableTicket);
+            // 
+            // int AvaialableTickets = TicketsOnSale ?? 0 ;
+            // 
+            // Console.WriteLine("AvaialableTicket = {0} ", AvaialableTicket);
+
+            // End Part 6-----------------------------------------------------------------------
+
+
+            /*|--------------------------------------------------
+              |                                                 |
+              |                     Part 6                      |
+              |          Implicit & Explicit conversion         |
+              |                                                 |             
+              |-------------------------------------------------|
+              |                                                 |        
+              |     Implicit conversion is done by compiler     |                                        |        
+              |                                                 |        
+              |                Pars & Try Parse                 |        
+              |                                                 |        
+              |-------------------------------------------------|*/
+
+
+            // float is a bigger datatype than int . so , no loss of data
+            // and Exceptions . Hence implicit conversion
+            // int i = 100;
+            // float f = i; 
+
+
+
+            // use explicit conversion using Convert class
+            // float f = 123.45F;
+
+            // use explicit conversion using cast () operator
+            // int i = (int)f; // Cast operator 
+            // int w = Convert.ToInt32(f);
+            // Console.WriteLine(f);
+
+
+
+            string strNumber = "100";
+            int i = int.Parse(strNumber);
+
+            int result = 0;
+
+            bool IsConversionSuccessful = int.TryParse(strNumber, out result);
+
+            if(IsConversionSuccessful)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+            // Difference between Parse and TryParse
+
+            // if the number is int a string format you have 2 options - Pars() & TryPars()
+
+            // parse () method throws an exception if it cannot parse the value  , whereas TryParse()
+            // returns a bool indicationg whether it succeeded or failed 
+
+
+            // Use Parse() if you are sure the value will be valid , itherwise use TryParse()
+
+
+
+
+            // End Part 6-----------------------------------------------------------------------
         }
     }
 }
