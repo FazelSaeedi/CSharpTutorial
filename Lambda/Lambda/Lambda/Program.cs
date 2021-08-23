f16f17819207c17bbd3eda38b811a325ef2d4bce
Fazel Saeedi <fazels36@gmail.com>
2021-08-23 14:40:40 +0430

    Lambda func action

---
+++ Lambda/Lambda/Lambda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            Action action = () => { } ;
+
+
+            i.ToString();
+
+            actionInt(99);
+            string s = func("Hi");
+
+        }
+
+        static Action<int> actionInt = myInt =>
+        {
+            var i = 0;
+            Console.WriteLine(myInt.ToString());
+        };
+
+        static Func<string, string> func = (x) =>
+        {
+            Console.WriteLine(x.ToString());
+            return x;
+        };
+    }
+}
